5aff48dd140a6fa382be630f8bde900d5af87f7f
Ivan Shabanov <Ivan.shabanov@gmail.com>
2025-03-05 14:59:54 +0700

    feat: проверка на bitix

--- scripts/content.js
+++ scripts/content.js
@@ -4,24 +4,23 @@ function remove_popup__error__log() {
 		_el.remove();
 	}
 }
+if (document.innerHTML.indexOf('bitrix') > 0) {
+	fetch("/__error.log", {
+		method: 'GET',
+		cache: "no-cache"
+	}).then(function (response) {
+		if (response.ok) {
+			let body = document.querySelector("body");
+			let div = document.createElement("div");
+			div.innerHTML = 'На сайте найден <a href="/__error.log" target="_blank">__error.log</a>';
+			div.classList.add('__error_log');
+			div.setAttribute('style', 'position:fixed; top: 0px; left: 0px; width: 100vw; text-align:center; color: #fff; padding: 10px; background: #a33; z-index: 1000000;');
+			body.append(div);
+			setTimeout(function () {
+				remove_popup__error__log()
+			}, 3000);
+		}
+	}).catch(function (err) {
 
-fetch("/__error.log", {
-    method: 'GET',
-	cache: "no-cache"
-}).then(function (response) {
-	if (response.ok) {
-		let body = document.querySelector("body");
-		let div = document.createElement("div");
-		div.innerHTML = 'На сайте найден <a href="/__error.log" target="_blank">__error.log</a>';
-		div.classList.add('__error_log');
-		div.setAttribute('style', 'position:fixed; top: 0px; left: 0px; width: 100vw; text-align:center; color: #fff; padding: 10px; background: #a33; z-index: 1000000;');
-		body.append(div);
-		setTimeout(function () {
-			remove_popup__error__log()
-		}, 3000);
-	}
-})
-.catch(function (err) {
-
-});
-
+	});
+}
